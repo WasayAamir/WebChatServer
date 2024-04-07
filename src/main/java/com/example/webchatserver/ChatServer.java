0e5578e8e001a5ed728dec19b5acf1477c66ff1a
Aakash <aakash.harendran@ontariotechu.net>
2024-04-06 22:36:52 -0400

    Display's code in welcome msg

--- src/main/java/com/example/webchatserver/ChatServer.java
+++ src/main/java/com/example/webchatserver/ChatServer.java
@@ -9,17 +9,22 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+//import static com.example.util.ChatAPIHandler.loadChatRoomHistory;
+//import static com.example.util.ChatAPIHandler.saveChatRoomHistory;
+
 @ServerEndpoint(value="/ws/{roomId}")
 public class ChatServer {
 
     private Map<String, String> usernames = new HashMap<String, String>();
     private static Map<String, String> roomList = new HashMap<String, String>();
+    private static Map<String, String> roomHistoryList = new HashMap<String, String>();
 
     @OnOpen
     public void open(@PathParam("roomId") String roomId, Session session) throws IOException {
         roomList.put(session.getId(), roomId); // adding userID to a room
 
-        session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server ): Welcome to the chat room. Please state your username to begin.\"}");
+
+        session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server): Welcome to chat room " + roomId + " Please state your username to begin.\"}");
     }
 
     @OnClose
@@ -30,6 +35,7 @@ public class ChatServer {
             usernames.remove(userId);
             String roomID = roomList.get(userId);
             roomList.remove(roomID);
+
 
             //broadcast this person left the server
             int countPeers = 0;
@@ -42,7 +48,6 @@ public class ChatServer {
             }
         }
     }
-
     @OnMessage
     public void handleMessage(String comm, Session session) throws IOException {
         String userID = session.getId();
@@ -52,27 +57,27 @@ public class ChatServer {
         String message = (String) jsonmsg.get("msg");
 
         // not their first message
-        if(usernames.containsKey(userID)){
+        if (usernames.containsKey(userID)) {
             String username = usernames.get(userID);
             System.out.println(username);
 
-            for(Session peer: session.getOpenSessions()){
+            for (Session peer : session.getOpenSessions()) {
                 // only broadcast messages to those in the same room
-                if(roomList.get(peer.getId()).equals(roomID)) {
+                if (roomList.get(peer.getId()).equals(roomID)) {
                     peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(" + username + "): " + message + "\"}");
                 }
             }
-        }else{ //first message is their username
+        } else { //first message is their username
             usernames.put(userID, message);
             session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server): Welcome, " + message + "!\"}");
 
             //broadcast this person joined the server to the rest
-            for(Session peer: session.getOpenSessions()){
-                if((!peer.getId().equals(userID)) && (roomList.get(peer.getId()).equals(roomID))){
+            for (Session peer : session.getOpenSessions()) {
+                if ((!peer.getId().equals(userID)) && (roomList.get(peer.getId()).equals(roomID))) {
                     peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server): " + message + " joined the chat room.\"}");
                 }
             }
         }
-
     }
+
 }
